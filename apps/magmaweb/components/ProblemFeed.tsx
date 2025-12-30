41fce30804b2d1088c99aacf9f056655d981c9b5
alpinaoki <alpine.naoki@gmail.com>
2025-12-31 00:33:56 +0900

    あ

--- apps/magmaweb/components/ProblemFeed.tsx
+++ apps/magmaweb/components/ProblemFeed.tsx
@@ -40,7 +40,7 @@ const [visible, setVisible] = useState(PAGE_SIZE)
     id,
     image_url,
     created_at,
-    profiles:profiles!profiles_user_id_fkey (
+    profiles:profiles!posts_user_id_fkey (
       username
     )
   `)
